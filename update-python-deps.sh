d4c575fa43a2f3aae8801b1562982535c53e952d
Janusz Piechówka <januszpiechowka@gmail.com>
2019-12-04 11:54:02 +0100

    Create update-python-deps.sh

---
+++ update-python-deps.sh
@@ -0,0 +1,3 @@
+pip3 list --format freeze | cut -d "=" -f 1 &> /tmp/python-dependencies.txt
+pip3 install --upgrade --upgrade-strategy eager -r /tmp/python-dependencies.txt
+rm /tmp/python-dependencies.txt
